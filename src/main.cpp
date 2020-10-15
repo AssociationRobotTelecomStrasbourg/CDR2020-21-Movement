bf18c3606ebb22b664f8653af569adfcbf2b2431
Arnaud Schlumberger <arn.schlumberger@gmail.com>
2020-10-15 17:22:00 +0200

    Ajout pilotage des servos-moteurs 1 & 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <Arduino.h>
 #include "pins_mvt.h"
 #include "turn_and_go.h"
+#include "PWMServo.h"
 
+PWMServo ServoGauche;
+PWMServo ServoDroite;
 TurnAndGo turn_and_go;
 float distance = -500;
+
+void ServosUp();
+void ServosDown();
 
 bool go = false;
 
@@ -12,6 +18,9 @@ int state = 0; //variable d'etat pour direction de mouvement du robot
 void setup() {
   Strat.begin(9600); //init canal comm Strat <-> mvt
   Serial.begin(9600);
+
+  ServoDroite.attach(SERVO2);
+  ServoGauche.attach(SERVO1);
   pinMode(13,OUTPUT);
 
   while(Strat.available()==0){
@@ -36,6 +45,7 @@ void setup() {
   digitalWrite(LED1,HIGH);
   digitalWrite(LED2,HIGH);
 
+  ServosUp();
   while(!go){
     //Attente du message GO
     if(Strat.available()>0 and Strat.read()==0x56){
@@ -59,21 +69,25 @@ void loop() {
     {
     case 0:
       turn_and_go.goTo(distance,0);
+      ServosDown();
       state++;
       break;
 
     case 1:
       turn_and_go.goTo(distance,distance);
       state++;
+      ServosUp();
       break;
 
     case 2:
       turn_and_go.goTo(0,distance);
+      ServosDown();
       state++;
       break;
 
     case 3:
       turn_and_go.goTo(0,0);
+      ServosUp();
       state=0;
       break;
     default:
@@ -82,4 +96,14 @@ void loop() {
     
     //distance = -distance;
   }
+}
+
+void ServosUp(){
+  ServoGauche.write(140);
+  ServoDroite.write(140);
+}
+
+void ServosDown(){
+  ServoGauche.write(50);
+  ServoDroite.write(50);
 }
